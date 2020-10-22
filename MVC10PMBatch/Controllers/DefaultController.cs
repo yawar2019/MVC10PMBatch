025d332bcb0d21f3fc8872efe81c0d81781edb75
yawar2019 <yawarali17@gmail.com>
2020-10-22 23:09:02 +0530

    Checkin Code

--- MVC10PMBatch/Controllers/DefaultController.cs
+++ MVC10PMBatch/Controllers/DefaultController.cs
@@ -20,5 +20,56 @@ namespace MVC10PMBatch.Controllers
 
             return View();
         }
+
+        public ActionResult GetEmployees()
+        {
+            List<EmployeeModel> emp = new List<EmployeeModel>();
+            
+
+            EmployeeModel obj1 = new EmployeeModel();
+            obj1.EmpId = 1;
+            obj1.EmpName = "Sai";
+            obj1.EmpSalary = 15000;
+
+            EmployeeModel obj2 = new EmployeeModel();
+            obj2.EmpId = 2;
+            obj2.EmpName = "Wilson";
+            obj2.EmpSalary = 15000;
+
+            EmployeeModel obj3 = new EmployeeModel();
+            obj3.EmpId = 1;
+            obj3.EmpName = "Gosh";
+            obj3.EmpSalary = 15000;
+
+            EmployeeModel obj4 = new EmployeeModel();
+            obj4.EmpId = 1;
+            obj4.EmpName = "Vali";
+            obj4.EmpSalary = 15000;
+
+            emp.Add(obj1);
+            emp.Add(obj2);
+            emp.Add(obj3);
+            emp.Add(obj4);
+
+
+            ViewBag.info = emp;
+            
+
+            return View();
+
+        }
+
+
+        public ActionResult EmployeeAnotherWay()
+        {
+            EmployeeModel obj = new EmployeeModel();
+            obj.EmpId = 1;
+            obj.EmpName = "Sai";
+            obj.EmpSalary = 15000;
+
+
+            //object model = obj;
+            return View(obj);
+        }
     }
 }

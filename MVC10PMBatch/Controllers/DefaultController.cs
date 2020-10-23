ef6e034ff6cf4f456e24acbebba39244c752141f
yawar2019 <yawarali17@gmail.com>
2020-10-23 22:52:42 +0530

    Comitted Code

--- MVC10PMBatch/Controllers/DefaultController.cs
+++ MVC10PMBatch/Controllers/DefaultController.cs
@@ -71,5 +71,97 @@ namespace MVC10PMBatch.Controllers
             //object model = obj;
             return View(obj);
         }
+
+        public ActionResult getAllEmployee()
+        {
+
+            List<EmployeeModel> emplist = new List<EmployeeModel>();
+
+
+            EmployeeModel obj1 = new EmployeeModel();
+            obj1.EmpId = 1;
+            obj1.EmpName = "Sai";
+            obj1.EmpSalary = 15000;
+
+            EmployeeModel obj2 = new EmployeeModel();
+            obj2.EmpId = 2;
+            obj2.EmpName = "Wilson";
+            obj2.EmpSalary = 15000;
+
+            EmployeeModel obj3 = new EmployeeModel();
+            obj3.EmpId = 1;
+            obj3.EmpName = "Gosh";
+            obj3.EmpSalary = 15000;
+
+            EmployeeModel obj4 = new EmployeeModel();
+            obj4.EmpId = 1;
+            obj4.EmpName = "Vali";
+            obj4.EmpSalary = 15000;
+
+            emplist.Add(obj1);
+            emplist.Add(obj2);
+            emplist.Add(obj3);
+            emplist.Add(obj4);
+
+
+            
+
+
+            //object model = obj;
+            return View(emplist);
+        }
+
+        public ActionResult getAllEmployeeDeptCustomize()
+        {
+
+            List<EmployeeModel> emplist = new List<EmployeeModel>();
+
+
+            EmployeeModel obj1 = new EmployeeModel();
+            obj1.EmpId = 1;
+            obj1.EmpName = "Sai";
+            obj1.EmpSalary = 15000;
+
+            EmployeeModel obj2 = new EmployeeModel();
+            obj2.EmpId = 2;
+            obj2.EmpName = "Wilson";
+            obj2.EmpSalary = 15000;
+
+            EmployeeModel obj3 = new EmployeeModel();
+            obj3.EmpId = 1;
+            obj3.EmpName = "Gosh";
+            obj3.EmpSalary = 15000;
+
+            EmployeeModel obj4 = new EmployeeModel();
+            obj4.EmpId = 1;
+            obj4.EmpName = "Vali";
+            obj4.EmpSalary = 15000;
+
+            emplist.Add(obj1);
+            emplist.Add(obj2);
+            emplist.Add(obj3);
+            emplist.Add(obj4);
+
+
+            DepartmentModel mobj = new Models.DepartmentModel();
+            mobj.DeptId = 1;
+            mobj.DeptName = "IT";
+
+            DepartmentModel mobj1 = new Models.DepartmentModel();
+            mobj1.DeptId = 2;
+            mobj1.DeptName = "Marketting";
+
+            List<DepartmentModel> deptObj = new List<DepartmentModel>();
+            deptObj.Add(mobj);
+            deptObj.Add(mobj1);
+
+            EmpDept listObj = new EmpDept();
+            listObj.Emp = emplist;
+            listObj.dept = deptObj;
+
+
+            //object model = obj;
+            return View(listObj);
+        }
     }
 }

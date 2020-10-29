6f3a047d8a227cd5c8b465c765a15dfb9f1354be
yawar2019 <yawarali17@gmail.com>
2020-10-29 23:06:13 +0530

    committed code

---
+++ AdonetExample/Controllers/NewEmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdonetExample.Models;
+
+namespace AdonetExample.Controllers
+{
+    public class NewEmployeeController : Controller
+    {
+        // GET: NewEmployee
+        EmployeeDetail db = new EmployeeDetail();
+        public ActionResult Index()
+        {
+            var Employees = db.GetEmployees();
+            return View(Employees);
+        }
+        [HttpGet]
+        public ActionResult Create() {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(EmployeeModel emp)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ AdonetExample/Models/EmployeeModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AdonetExample.Models
+{
+    public class EmployeeModel
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int EmpSalary { get; set; }
+    }
+
+    public class EmployeeDetail
+    {
+        SqlConnection con = new SqlConnection("Data Source=AZAM-PC\\SQLEXPRESS;Initial Catalog=MVC10PMBatchDb;Integrated Security=true");
+
+        public List<EmployeeModel> GetEmployees() {
+
+            List<EmployeeModel> listemp = new List<EmployeeModel>();
+            SqlCommand cmd = new SqlCommand("spr_EmployeeDetails",con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                EmployeeModel emp = new EmployeeModel();
+                emp.EmpId = Convert.ToInt32(dr[0]);
+                emp.EmpName = Convert.ToString(dr[1]);
+                emp.EmpSalary = Convert.ToInt32(dr[2]);
+                listemp.Add(emp);
+            }
+            return listemp;
+        }
+    }
+}

d33de2157f6dd7c2b161c0e8985d1e54e30e5e0c
yawar2019 <yawarali17@gmail.com>
2020-11-07 22:47:37 +0530

    commited code

---
+++ CodeFirstApprocah/Controllers/DefaultController.cs
@@ -0,0 +1,40 @@
+﻿using CodeFirstApprocah.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeFirstApprocah.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        EmployeeContext db = new EmployeeContext();
+        public ActionResult Index()
+        {
+            return View(db.EmployeeModels.ToList());
+        }
+
+        public ActionResult Create() {
+            return View();
+               
+        }
+        [HttpPost]
+        public ActionResult Create(EmployeeModel emp)
+        {
+            db.EmployeeModels.Add(emp);
+            int i = db.SaveChanges();
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+
+            }
+
+        }
+    }
+}
---
+++ CodeFirstApprocah/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodeFirstApprocah.MvcApplication" Language="C#" %>
